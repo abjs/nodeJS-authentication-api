025c77f6c798d78d7305073bb07ba61f83b9d299
Abin Joseph <73039692+abjs@users.noreply.github.com>
2020-12-01 20:09:59 +0530

    Mogo url

--- helpers/init_mogodb.js
+++ helpers/init_mogodb.js
@@ -1,6 +1,14 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tlroa.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 const mongoose = require('mongoose');
 mongoose
-.connect(process.env.MONGODB_URL,{
+.connect(uri,{
     dbName: process.env.MONGODB_DBNAME,
     useNewUrlParser: true ,
     useUnifiedTopology: true ,
